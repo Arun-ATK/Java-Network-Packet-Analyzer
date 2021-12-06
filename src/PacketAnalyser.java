0a6cb9cb2ba7d71e08b3349dfdb5796c5a639b35
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2021-12-06 22:20:07 +0530

    Starting packet capture (winpcap)

--- src/PacketAnalyser.java
+++ src/PacketAnalyser.java
@@ -2,9 +2,6 @@ import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PacketAnalyser {
     public static void main(String[] args) {
@@ -16,52 +13,19 @@ public class PacketAnalyser {
     }
 
     private static void startup() throws IOException{
-        List<String> command = new ArrayList<>();
         OsCheck.OSType osType = OsCheck.getOperatingSystemType();
         System.out.println("OS: " + osType);
 
-        String shell;
         if (osType == OsCheck.OSType.Windows) {
-            command.add("cmd.exe");
-            command.add("/c");
+            Runtime.getRuntime().exec("cmd.exe /C start net start npf");
+
+            System.out.println("Started packet capture");
         }
         else {
             System.out.println("Non-windows system not supported!");
             System.exit(-1);
         }
 
-        try {
-            Process cmdPrompt = Runtime.getRuntime().exec("cmd.exe /C start dir");
-            cmdPrompt.destroy();
-            System.out.println("ok");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//        InputStream inputStream = null;
-//        InputStream errorStream = null;
-
-//        try {
-//            ProcessBuilder processBuilder = new ProcessBuilder(command);
-//            Process process = processBuilder.start();
-//
-//            inputStream = process.getInputStream();
-//            errorStream = process.getErrorStream();
-//
-//            System.out.println("Process InputStream: " + inputStream.toString());
-//            System.out.println("Process ErrorStream: " + errorStream.toString());
-//
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (inputStream != null) {
-//                inputStream.close();
-//            }
-//            if (errorStream != null) {
-//                errorStream.close();
-//            }
-//        }
     }
 }

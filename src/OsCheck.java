e419889079d320a36ef1807ba767345ad58f56dd
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2021-12-06 20:35:28 +0530

    Added temp code for starting up packet capture

---
+++ src/OsCheck.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+
+/**
+ * THANKS TO bastengao (https://gist.github.com/bastengao)
+ * Helper class to check the operating system
+ */
+
+public class OsCheck {
+    // Type of Operating systems
+    public enum OSType {
+        Windows,
+        MacOS,
+        Linux,
+        Other
+    }
+
+    protected static OSType detectedOS;
+
+    /**
+     * detected the operating system from the os.name System property and cache
+     * the result
+     *
+     * RETURN VALUE - the operating system detected
+     */
+    public static OSType getOperatingSystemType() {
+        if (detectedOS == null) {
+            String OS = System.getProperty("os.name", "generic").toLowerCase();
+            if (OS.contains("win")) {
+                detectedOS = OSType.Windows;
+            }
+            else if ((OS.contains("mac")) || (OS.contains("darwin"))) {
+                detectedOS = OSType.MacOS;
+            }
+            else if (OS.contains("nux")) {
+                detectedOS = OSType.Linux;
+            }
+            else {
+                detectedOS = OSType.Other;
+            }
+        }
+
+        return detectedOS;
+    }
+}
--- src/PacketAnalyser.java
+++ src/PacketAnalyser.java
@@ -1,24 +1,67 @@
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PacketAnalyser {
     public static void main(String[] args) {
+        try {
+            startup();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        StringBuilder errbuf = new StringBuilder();
-        List<PcapIf> ifs = new ArrayList<>();
-        int statusCode = Pcap.findAllDevs(ifs, errbuf);
-        if (statusCode != Pcap.OK || ifs.isEmpty()) {
-            System.out.println("Error occurred: " + errbuf);
-            return;
+    private static void startup() throws IOException{
+        List<String> command = new ArrayList<>();
+        OsCheck.OSType osType = OsCheck.getOperatingSystemType();
+        System.out.println("OS: " + osType);
+
+        String shell;
+        if (osType == OsCheck.OSType.Windows) {
+            command.add("cmd.exe");
+            command.add("/c");
+        }
+        else {
+            System.out.println("Non-windows system not supported!");
+            System.exit(-1);
         }
 
-        System.out.println("No error");
-        System.out.println(ifs.size());
-        for (PcapIf p : ifs) {
-            System.out.println(p.toString());
+        try {
+            Process cmdPrompt = Runtime.getRuntime().exec("cmd.exe /C start dir");
+            cmdPrompt.destroy();
+            System.out.println("ok");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+//        InputStream inputStream = null;
+//        InputStream errorStream = null;
+
+//        try {
+//            ProcessBuilder processBuilder = new ProcessBuilder(command);
+//            Process process = processBuilder.start();
+//
+//            inputStream = process.getInputStream();
+//            errorStream = process.getErrorStream();
+//
+//            System.out.println("Process InputStream: " + inputStream.toString());
+//            System.out.println("Process ErrorStream: " + errorStream.toString());
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (inputStream != null) {
+//                inputStream.close();
+//            }
+//            if (errorStream != null) {
+//                errorStream.close();
+//            }
+//        }
     }
 }

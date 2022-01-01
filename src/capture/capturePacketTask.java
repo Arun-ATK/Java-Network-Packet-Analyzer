e5f34deca3a32ffb88751290c5c4f045ffd7af50
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2022-01-01 20:43:04 +0530

    calling parseRawPacket in new capture implementation

--- src/capture/capturePacketTask.java
+++ src/capture/capturePacketTask.java
@@ -1,14 +1,8 @@
 package capture;
 
-import org.jnetpcap.ByteBufferHandler;
 import org.jnetpcap.Pcap;
 import org.jnetpcap.nio.JMemory;
 import org.jnetpcap.packet.PcapPacket;
-import org.jnetpcap.packet.PcapPacketHandler;
-import org.jnetpcap.packet.PeeringException;
-
-import java.io.PrintStream;
-import java.util.Date;
 
 public class capturePacketTask implements Runnable {
     PacketCapturer packetCapturer;
@@ -21,41 +15,26 @@ public class capturePacketTask implements Runnable {
 
     @Override
     public void run() {
-//        ByteBufferHandler<PrintStream> byteBufferHandler = (pcapHeader, byteBuffer, printStream) -> {
-//            PcapPacket packet = new PcapPacket(pcapHeader, byteBuffer);
-//            packetCapturer.parseRawPacket(packet);
-//
-//            if (Thread.currentThread().isInterrupted()) {
-//                pcap.breakloop();
-//            }
-//        };
-//
-//
-//        PcapPacketHandler<PrintStream> pcapPacketHandler = new PcapPacketHandler<PrintStream>() {
-//            @Override
-//            public void nextPacket(PcapPacket pcapPacket, PrintStream printStream) {
-//                printStream.println("Hello");
-//            }
-//        };
-//
-//        int cnt = 2;
-//        PrintStream out = System.out;
-//        int stat = pcap.loop(cnt, byteBufferHandler, out);
-
         PcapPacket packet = new PcapPacket(JMemory.POINTER);
-        int stat;
         boolean cont = true;
         while (cont) {
-            stat = pcap.nextEx(packet);
+            int stat = pcap.nextEx(packet);
 
             switch (stat) {
-                case Pcap.NEXT_EX_OK -> System.out.println("OK");
+                case Pcap.NEXT_EX_OK -> {
+                    PcapPacket p = new PcapPacket(packet);
+                    packetCapturer.parseRawPacket(p);
+                }
                 case Pcap.NEXT_EX_EOF -> {
-                    System.out.println("EOF");
+                    System.out.println("File ended");
+                    cont = false;
+                }
+                case Pcap.NEXT_EX_NOT_OK -> {
+                    System.out.println("Some error occurred");
                     cont = false;
                 }
                 default -> {
-                    System.out.println("Umm... Idk");
+                    System.out.println("WHAT: " + stat);
                     cont = false;
                 }
             }
